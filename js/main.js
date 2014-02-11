1ac92cadecc03d3f0e9c59c55e7a450e067be1d0
kunaldpatel <kunal.d.patel@gmail.com>
2014-02-10 19:43:04 -0500

    Updated gfx and some copy changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 // Inspired by Michael Wang's clever implementation - mkwng.com 
-var bait = $('.bait');
 // Javascript has no native assoc. array, so let's use an Object instead to hold my answers. Naming this way, rather than via numbers, allows for easier re-arranging of the copy.
-var answers = new Object;
+var answers = new Object({});
 answers.arch = "in architecture, which taught me to balance design with construction";
-answers.swiss = "from coding prototypes to running a UX School";
-answers.dunce = "as long as they help lead to the right answer";
+answers.yes = "from coding prototypes to running a UX School";
+answers.dumb = "as long as they help lead to the right answer";
 answers.huge = "as an Experience Lead at Huge, after 3.5 years";
-answers.silly = "best explained over a drink"
+answers.ideas = "best explained over a drink (tea, perhaps?)";
 answers.hero = "saving lives, but a designer trying to make them easier";
 
 // Michael's function for wrapping the original element in a span and removing its contents
@@ -32,7 +31,7 @@ function replaceEmail(target) {
     var c = ".is";
     var h = 'mailto:' + e + a + d + c;
     $(target).attr('href', h);
-};
+}
 
 function updateImg(target) {
     // Hide current image
@@ -66,12 +65,12 @@ $(document).ready(function() {
         // Whatever answer is currently selected, remove the styles
         $('span.answer.current').removeClass('current active-hover-fix');
         // If we're on the currently selected answer
-        if ((key + "-svg") == $('object.active').attr('id')) {
+        if ((key + "-svg") === $('object.active').attr('id')) {
             // Replace image with default
             updateImg('hi');
         }
         // Otherwise, if this is a different answer
-        else 
+        else
         {
             // Apply selected style to this span.answer
             $(this).addClass('current').on('mouseleave', function(e) {
